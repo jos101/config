de16d63d1eb6df528d50bc59cb5411082d3988e2
Your Name <you@example.com>
2024-10-22 16:36:49 -0500

    lazyvim

---
+++ config/windows/lazyvim/appdata/local/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,8 @@
+return {
+  -- change trouble config
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    -- opts will be merged with the parent spec
+    opts = { window = { position = "right" } },
+  },
+}
