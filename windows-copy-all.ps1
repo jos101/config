0cd9a8005b49a78333765a293a5a7fb40c67d503
Your Name <you@example.com>
2025-05-28 07:19:25 -0500

    agrega script para copiar las configuraciones en windows

---
+++ windows-copy-all.ps1
@@ -0,0 +1,20 @@
+$dir = $pwd 
+Write-host $dir
+
+Write-host "copiando lazyvim"
+cd config/windows/lazyvim 
+#cp appdata ~/
+
+Write-host "copiando .wezterm.lua"
+cd $dir/config/windows/profile
+cp .wezterm.lua ~/ 
+
+Write-host "copiando profile powershell"
+if ( !(Test-Path $profile) ){
+  New-Item -Path $profile -ItemType File -Force
+} 
+cp $dir/powershell/Microsoft.PowerShell_profile.ps1 ~/app
+
+
+cd $dir
+
